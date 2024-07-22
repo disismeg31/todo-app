7a38039237b3ab923e62b1be89fcd99097981cff
Megha KL <megha@nortvis.com>
2024-07-22 23:28:46 +0530

    basic finish

--- src/components/ToDoInp.jsx
+++ src/components/ToDoInp.jsx
@@ -1,42 +1,59 @@
 import React, { useState } from "react";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 import ToDos from "./ToDos.jsx";
 
 function ToDoInp() {
   // While not strictly necessary, including ids in your initial state is a good practice for consistency, future-proofing, and efficient list rendering in React.
   const [tasks, setTasks] = useState([
-    { id: uuidv4(), title: "Eat breakfast", priority: "High" },
+    // { id: uuidv4(), title: "Eat breakfast", priority: "High" },
   ]);
   const [newTask, setNewTask] = useState({
     title: "",
     priority: "",
   });
 
+  const [update, setUpdate] = useState(true);
+  const [edit, setEdit] = useState(null);
+
   function handleInputChange(e) {
     const { name, value } = e.target;
-    setNewTask((n) => ({ ...n, [name]: value}));
+    setNewTask((n) => ({ ...n, [name]: value }));
   }
 
   function addTask() {
-      // console.log('newTask before adding:', newTask);
-    if(newTask.title.trim() !== "" && newTask.priority.trim() !== ""){
-      const taskWithId = {...newTask,id: uuidv4()}
-    setTasks((t) =>{ 
-      console.log('Adding task',taskWithId);
-      return [...t, taskWithId] 
-    } );
-    setNewTask({title: "", priority: ""});
-    }
-    else{
-      alert("todo and priority fields are required !!!");
+    // console.log('newTask before adding:', newTask);
+    if (!update) {
+      setTasks(
+        tasks.map((editEle) => {
+          if (editEle.id === edit) {
+            return {
+              ...editEle,
+              title: newTask.title,
+              priority: newTask.priority,
+            };
+          }
+          return editEle;
+        })
+      );
+      setUpdate(true); //because Add button is displayed when update is true
+      setNewTask({
+        title: "",
+        priority: "",
+      });
+      setEdit(null); //to reset edit id so that it doesnt spread to next item
+    } else {
+      if (newTask.title.trim() !== "" && newTask.priority.trim() !== "") {
+        const taskWithId = { ...newTask, id: uuidv4() , complete:false };
+        setTasks((t) => {
+          console.log("Adding task", taskWithId);
+          return [...t, taskWithId];
+        });
+        setNewTask({ title: "", priority: "" });
+      } else {
+        alert("todo and priority fields are required !!!");
+      }
     }
   }
-  // console.log(tasks);
-
-  // function deleteTask(index) {
-  //   const upatedTasks = tasks.filter((_, i) => i !== index);
-  //   setTasks(upatedTasks);
-  // }
 
   return (
     <>
@@ -53,22 +70,39 @@ function ToDoInp() {
           className="mx-2 my-2 h-14 p-4 border-0 rounded-lg text-slate-800"
           name="priority"
           id="priority"
+          value={newTask.priority} // set value to newTask.priority
           onChange={handleInputChange}
-        >  
-          <option value="">Select Priority</option>
+        >
+          <option disabled hidden value="">
+            Select Priority
+          </option>
           <option value="Low">Low</option>
           <option value="Medium">Medium</option>
           <option value="High">High</option>
-          
         </select>
-        <button
-          className="h-14 w-24 p-4 mx-2 my-2 text-white bg-slate-500 hover:bg-slate-600 active:bg-slate-700 rounded-lg "
-          onClick={addTask}
-        >
-          Add
-        </button>
+        {update ? (
+          <button
+            className="h-14 w-24 p-4 mx-2 my-2 text-white bg-slate-500 hover:bg-slate-600 active:bg-slate-700 rounded-lg "
+            onClick={addTask}
+          >
+            Add
+          </button>
+        ) : (
+          <button
+            className="h-14 w-24 p-4 mx-2 my-2 text-white bg-slate-500 hover:bg-slate-600 active:bg-slate-700 rounded-lg "
+            onClick={addTask}
+          >
+            Update
+          </button>
+        )}
       </div>
-      <ToDos tasks={tasks} setTasks={setTasks} />
+      <ToDos
+        tasks={tasks}
+        setTasks={setTasks}
+        setNewTask={setNewTask}
+        setUpdate={setUpdate}
+        setEdit={setEdit}
+      />
     </>
   );
 }
--- src/components/ToDos.jsx
+++ src/components/ToDos.jsx
@@ -1,79 +1,90 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React from "react";
 import { AiOutlineEdit } from "react-icons/ai";
 import { TiDeleteOutline } from "react-icons/ti";
-function ToDos({tasks,setTasks}) {
-
-  function deleteTask(index) {
-    const upatedTasks = tasks.filter((_, i) => i !== index);
-    setTasks(upatedTasks);
+function ToDos({ tasks, setTasks, setNewTask, setUpdate, setEdit }) {
+  function deleteTask(id) {
+    const isConfirm = window.confirm("areyou sure you want to remove it")
+    if(isConfirm){
+      const upatedTasks = tasks.filter((item) => id !== item.id);
+      setTasks(upatedTasks);
+    }  
   }
 
+  function editTask(id) {
+    const findItem = tasks.find((task) => {
+      return id === task.id;
+    });
+    setNewTask(findItem);
+    setUpdate(false);
+    setEdit(id);
+    console.log(findItem);
+  }
 
+  function handleRemoveAll(){
+    setTasks([])
+  }
 
+  function handleComplete(id){
+    setTasks(tasks.map((compItem)=>{
+      if(compItem.id == id){
+        return {...compItem, complete:!compItem.complete}
+      }
+      return compItem;
+    }))
+  }
 
   return (
-    <div className='todos'>
-      <ul> 
-        {
-          tasks && tasks.map((task)=>(
-            <li key={task.id} className={`flex justify-between items-center rounded-lg border-b-1 px-5 py-1 m-2 h-16   ${task.priority === 'High'? 'bg-[#fa828c]' :task.priority === 'Medium'? 'bg-[#fff281]':'bg-[#bae777]'}`}>
-            <div className='flex gap-3 items-center'>
-              <span className='flex items-center'>
-                <input className='h-4 w-4' type="checkbox" />
-              </span>
-              <span>{task.title}</span>
-            </div>
-            <div className='flex gap-3'>
-              <span className='cursor-pointer'>
-                <AiOutlineEdit size={25} />
-              </span>
-              <span className='cursor-pointer' onClick={()=>deleteTask(task.id)}>
-                <TiDeleteOutline size={25} />
-              </span>
-            </div>
-          </li>
-          ))
-        }
-        
-        {/* <li className='flex justify-between items-center rounded-lg border-b-1 px-5 py-1 m-2 h-16 bg-[#fff281]'>
-          <div className='flex gap-3 items-center'>
-            <span className='flex items-center'>
-              <input className='h-4 w-4' type="checkbox" />
-            </span>
-            <span>Task 2</span>
-          </div>
-          <div className='flex gap-3'>
-            <span className='cursor-pointer'>
-              <AiOutlineEdit size={25} />
-            </span>
-            <span className='cursor-pointer'>
-              <TiDeleteOutline size={25} />
-            </span>
-          </div>
-        </li> */}
-        {/* <li className='flex justify-between items-center rounded-lg border-b-1 px-5 py-1 m-2 h-16 bg-[#fa828c]'>
-          <div className='flex gap-3 items-center'>
-            <span className='flex items-center'>
-              <input className='h-4 w-4' type="checkbox" />
-            </span>
-            <span>Task 2</span>
-          </div>
-          <div className='flex gap-3'>
-            <span className='cursor-pointer'>
-              <AiOutlineEdit size={25} />
-            </span>
-            <span className='cursor-pointer'>
-              <TiDeleteOutline size={25} />
-            </span>
-          </div>
-        </li> */}
+    <div className="todos">
+      <ul>
+        {tasks &&
+          tasks.map((task) => (
+            <li
+              key={task.id}
+              className={`flex justify-between items-center rounded-lg border-b-1 px-5 py-1 m-2 h-16  ${
+                task.priority === "High"
+                  ? "bg-[#fa828c]"
+                  : task.priority === "Medium"
+                  ? "bg-[#fff281]"
+                  : "bg-[#bae777]"
+              }`}
+            >
+              <div className="flex gap-3 items-center">
+                <span className="flex items-center">
+                  <input className="h-4 w-4" type="checkbox" onClick={()=>handleComplete(task.id)}/>
+                </span>
+                <span className={`${task.complete?'line-through':""} `}>{task.title}</span>
+              </div>
+              <div className="flex gap-3">
+                <span
+                  className="cursor-pointer"
+                  onClick={() => editTask(task.id)}
+                >
+                  <AiOutlineEdit size={25} />
+                </span>
+                <span
+                  className="cursor-pointer"
+                  onClick={() => deleteTask(task.id)}
+                >
+                  <TiDeleteOutline size={25} />
+                </span>
+              </div>
+            </li>
+          ))}
       </ul>
+      {
+      tasks.length>0 &&
+      <button className="h-14 p-4 mx-2 my-2 text-white bg-slate-500 hover:bg-slate-600 active:bg-slate-700 rounded-lg "
+      onClick={handleRemoveAll}>
+       Remove All
+     </button>
+     }
+      
     </div>
-  )
+  );
 }
 
 export default ToDos;
 
 // low :bg-[#bae777], high :bg-[#fa828c] , medium : bg-[#fff281] - priority
-// edit and delete  
+// edit and delete
